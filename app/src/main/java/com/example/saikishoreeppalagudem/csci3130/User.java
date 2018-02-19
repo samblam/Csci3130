7f6f1df53b3120a24e36210570ba674a5c1c5e00
Samblam <sam.barefoot@dal.ca>
2018-02-19 17:42:22 -0400

    Adding Branch for login

---
+++ app/src/main/java/com/example/saikishoreeppalagudem/csci3130/User.java
@@ -0,0 +1,20 @@
+package com.example.saikishoreeppalagudem.csci3130;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//Created by Sam 2018-02-19
+
+public class User{
+
+    private String pass;
+    private String email;
+    private int login_state;
+
+    public User(String email, String pass, int login) {
+
+
+
+    }
+
+}
