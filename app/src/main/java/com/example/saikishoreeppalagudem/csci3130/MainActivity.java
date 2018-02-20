ba2e89d44f842d54d19cb23a8207a6ac53187215
Samblam <sam.barefoot@dal.ca>
2018-02-19 20:54:43 -0400

    User class created, test to see that input is saved correctly. Next step is writing user info to a text file

--- app/src/main/java/com/example/saikishoreeppalagudem/csci3130/MainActivity.java
+++ app/src/main/java/com/example/saikishoreeppalagudem/csci3130/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.saikishoreeppalagudem.csci3130;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button register = findViewById(R.id.button2);
+        final EditText Password = findViewById(R.id.editText2);
+        final Button signin = findViewById(R.id.button);
+        final EditText login = findViewById(R.id.editText);
+        final TextView message = findViewById(R.id.textView);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                User current_user = new User(String.valueOf(login.getText()), String.valueOf(Password.getText()), true);
+                message.setText("The User info is " + current_user.getLogin() + " " + current_user.getPass() + " " + current_user.getLogin_state());
+
+            }
+
+            });
     }
 }
+
+
+
+
+
+
+
+
+
