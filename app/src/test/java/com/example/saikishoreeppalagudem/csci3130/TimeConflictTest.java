37151ce05ad9bf48fff56ecb039c8e05696c3e4b
ESKishore <saikishore.eppalagudem@gmail.com>
2018-03-06 23:25:55 -0400

    JUnit Test for TimeConflict

---
+++ app/src/test/java/com/example/saikishoreeppalagudem/csci3130/TimeConflictTest.java
@@ -0,0 +1,46 @@
+package com.example.saikishoreeppalagudem.csci3130;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by saikishoreeppalagudem on 2018-03-06.
+ */
+
+public class TimeConflictTest {
+    private static String testInput0 = "1035-1125";
+    private static String testInput1 = "0835-0935";
+    private static ArrayList<String> testScheduleInput0 = new ArrayList<>(Arrays.asList("1035-1155","1425-1555"));
+    private static String testOutput0 = "Time Conflict!";
+    private static String testOutput1 = "No time conflict!";
+
+
+    private static boolean doTest( String input, ArrayList<String> scheduleInput, String output ) {
+        TimeConflict timeConflict = new TimeConflict();
+        String al = timeConflict.checkTimeConflict(input, scheduleInput);
+        System.out.println( "Input: " );
+        System.out.println( input );
+        System.out.println("Schedule Input");
+        System.out.println(scheduleInput);
+        System.out.println( "Generated output" );
+        System.out.println( al );
+        System.out.println( "Expected output" );
+        System.out.println( output );
+        System.out.println( "---------------------------------------------------" );
+        return al == output;
+    }
+
+    @Test
+    public void testFail() {
+        assertTrue( "Time conflict test" , doTest( testInput0, testScheduleInput0, testOutput0 ));
+    }
+
+    @Test
+    public void testPass(){
+        assertTrue("No time conflict test", doTest(testInput1, testScheduleInput0, testOutput1));
+    }
+}
