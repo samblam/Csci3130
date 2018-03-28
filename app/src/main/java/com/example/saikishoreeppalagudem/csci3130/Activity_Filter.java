db1bff1b0109ee43d9f6e0819d94e5c55170a28f
Manojha18 <35583244+Manojha18@users.noreply.github.com>
2018-03-28 12:37:43 -0300

    Filter activity created

---
+++ app/src/main/java/com/example/saikishoreeppalagudem/csci3130/Activity_Filter.java
@@ -0,0 +1,13 @@
+package com.example.saikishoreeppalagudem.csci3130;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Filter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__filter);
+    }
+}
